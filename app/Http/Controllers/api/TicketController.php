1257b46f63676963e2ecf2b1762b76b4109bb2fc
Arthur <r.thur.light@gmail.com>
2023-11-09 21:53:53 +0000

    changes

--- app/Http/Controllers/api/TicketController.php
+++ app/Http/Controllers/api/TicketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Events\BorneEvent;
 use App\Http\Controllers\Controller;
 use App\Models\Service;
+use App\Models\Ticket;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -36,7 +37,7 @@ class TicketController extends Controller
             ->groupBy('services.id')
             ->first();
         if ($service) {
-            $lastTicket = \App\Models\Ticket::query()
+            $lastTicket = Ticket::query()
             ->whereHas('Service', function ($query) use ($service) {
                 $query->where('id', $service->id);
             })
@@ -50,7 +51,7 @@ class TicketController extends Controller
                 $queue++;
             }
 
-            $newTicket = new \App\Models\Ticket();
+            $newTicket = new Ticket();
             $newTicket->code = mt_rand(1111,9999);
             $newTicket->numero = $queue;
             $newTicket->service = $service->nom;
@@ -76,5 +77,40 @@ class TicketController extends Controller
             return response()->json(["message"=> "Service non trouvé", "status" => false,]);
         }
         
+    }
+
+    public function show($id) {
+        $ticket = Ticket::find($id);
+        $today = Carbon::today()->toDateString();
+
+        if ($ticket) {
+            $service = Service::query()
+                //->select('services.id', 'services.nom', 'services.photo', 'services.temps', 'services.prefix')
+                ->where('services.id', $ticket->service_id)
+                ->selectRaw('services.id, services.nom, services.photo, services.prefix, services.temps_moyen, COUNT(DISTINCT tickets.id) AS restant')
+                ->leftJoin('tickets', function ($join) use ($today, $ticket) {
+                    $join->on('services.id', '=', 'tickets.service_id')
+                        ->where('tickets.etat', '=', 0)
+                        ->where('tickets.numero', '<', $ticket->numero)
+                        ->whereBetween('tickets.created_at', [$today, now()]);
+                })
+                ->groupBy('services.id')
+                ->first();
+
+            $averageTimeInMillis = $service->temps_moyen * $service->restant;
+            $averageTimeInSeconds = $averageTimeInMillis / 1000;
+
+            $init = $averageTimeInSeconds;
+            $day = floor($init / 86400);
+            $hours = floor(($init -($day*86400)) / 3600);
+            $minutes = floor(($init / 60) % 60);
+            //$seconds = $init % 60;
+            $average =  "$hours H $minutes MN";
+
+            $passage = Carbon::now()->addSeconds($averageTimeInSeconds);
+
+            return response()->json(["message"=> "Ticket trouve", "ticket" => $ticket, "status" => true,  "average" => $average, "passage" => $passage->format('H:i')]);
+        }
+        return response()->json(["message"=> "Ticket non trouve", "status" => false]);
     }
 }
